9bf3088d7ed32f0f85a5c905294e4098a00f558e
seek-geek <2363550687@qq.com>
2015-11-24 10:52:00 +0800

    test 1

--- app/src/main/java/com/benkids/my_application/MainActivity.java
+++ app/src/main/java/com/benkids/my_application/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
 public class MainActivity extends ActionBarActivity {
-
+    //ceshi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
