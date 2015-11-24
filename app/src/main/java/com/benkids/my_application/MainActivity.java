da26fd0b617458073a9a0d418beac7123ebab1d6
seek-geek <2363550687@qq.com>
2015-11-24 10:45:25 +0800

    Initial commit

---
+++ app/src/main/java/com/benkids/my_application/MainActivity.java
@@ -0,0 +1,13 @@
+package com.benkids.my_application;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
